17256b3a83f571fbd48db894792f9cd5f96f9d57
Atik101 <>
2021-05-06 09:17:07 +0000

    sum of two number

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main(){
+  int num1,num2;
+  num1=10;
+  num2=20;
+  int sum=num1+num2;
+  cout<<"Sum is :"<<sum;
+  return 0;
 }
